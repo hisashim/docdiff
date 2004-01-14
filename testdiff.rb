183194ae914be07909dc57c9a991c24e8e01fa4f
Hisashi Morita <hisashim@workbook.org>
2004-01-14 11:51:45 +0000

    Added testdiff.rb, although this is not really a test script but a script to study akr diff.

---
+++ testdiff.rb
@@ -0,0 +1,35 @@
+require "diff"
+require "pp"
+
+#a = "Hi, I am matz.  I am Ruby's father.".split
+#b = "Hello, I am eban.  I am just another Ruby porter.".split
+
+a = [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j]
+b = a.reverse
+b = [:a, :b, :x, :y, :e, :f, :g, :h, :i, :j]
+
+diff = Diff.new(a, b)
+#pp a
+#pp b
+#pp diff.lcs(:speculative)
+#diff.ses(:speculative)
+diff.ses(:contours)
+#diff.ses(:shortestpath)
+
+=begin
+pp diff.lcs(:shortestpath)
+pp diff.ses(:shortestpath)
+
+pp diff.lcs(:shortestpath)
+pp diff.ses(:shortestpath)
+=end
+
+=begin
+a = "Hi, I am matz.  I am Ruby's father.".split(//)
+b = "Hello, I am eban.  I am just another Ruby porter.".split(//)
+diff = Diff.new(a, b)
+pp a
+pp b
+pp diff.lcs(:speculative)
+pp diff.ses(:speculative)
+=end
