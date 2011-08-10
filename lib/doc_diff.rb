a831f0d0b029168347ccedb51f1b5fa17f7b0c41
Kakutani Shintaro <shintaro@kakutani.com>
2011-08-10 16:07:20 +0900

    introduce module Docdiff

--- lib/doc_diff.rb
+++ lib/doc_diff.rb
@@ -1,7 +1,6 @@
 # DocDiff: word/character-oriented text comparison utility
 # Copyright (C) 2002-2011 Hisashi MORITA
 # Requirements: Ruby (>= 1.8)
-require 'docdiff/version'
 class DocDiff
 
   AppVersion = Docdiff::VERSION
---
+++ lib/docdiff.rb
@@ -0,0 +1,7 @@
+# DocDiff: word/character-oriented text comparison utility
+# Copyright (C) 2002-2011 Hisashi MORITA
+# Requirements: Ruby (>= 1.8)
+require 'docdiff/version'
+require 'doc_diff'
+module Docdiff
+end
