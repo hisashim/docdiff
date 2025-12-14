ba639b8f1ef5686ab928a570599f6b097dbb79e8
Hisashi Morita <hisashim@icloud.com>
2025-12-14 10:26:18 +0900

    Remove redundant DocDiff::AppVersion in favor of Docdiff::VERSION

--- lib/doc_diff.rb
+++ lib/doc_diff.rb
@@ -7,7 +7,6 @@ require 'docdiff/view'
 
 class DocDiff
 
-  AppVersion = Docdiff::VERSION
   Author = "Copyright (C) 2002-2011 Hisashi MORITA.\n" +
            "diff library originates from Ruby/CVS by TANAKA Akira.\n"
   License = "This software is licensed under so-called modified BSD license.\n" +
