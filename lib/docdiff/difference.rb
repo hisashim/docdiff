f6d9be16f1d0077223e3697f743ea2958b178cd7
Hisashi Morita <hisashim@icloud.com>
2025-12-28 01:32:01 +0900

    Lint: Use elsif

--- lib/docdiff/difference.rb
+++ lib/docdiff/difference.rb
@@ -30,33 +30,36 @@ class DocDiff
       @raw_list.each_with_index do |block, i|
         case block.first
         when :common_elt_elt
-          if i == 0                       # first block
+          if i == 0
+            # first block
             self << block
-          else                            # in-between or the last block
-            if @raw_list[i - 1].first == :del_elt  # previous block was del
-              self << @raw_list[i - 1]
-              self << block
-            else                                   # previous block was add
-              self << block
-            end
+          elsif @raw_list[i - 1].first == :del_elt
+            # previous block was del
+            self << @raw_list[i - 1]
+            self << block
+          else
+            # previous block was add
+            self << block
           end
         when :del_elt
-          if i == (@raw_list.size - 1)    # last block
+          if i == (@raw_list.size - 1)
+            # last block
             self << block
-          else                            # first block or in-between
+          else
             # do nothing, let the next block to decide what to do
           end
         when :add_elt
-          if i == 0                       # first block
+          if i == 0
+            # first block
             self << block
-          else                            # in-between or the last block
-            if @raw_list[i - 1].first == :del_elt  # previous block was del
-              deleted = @raw_list[i - 1][1]
-              added   = @raw_list[i][2]
-              self << [:change_elt, deleted, added]
-            else                                   # previous block was common
-              self << block
-            end
+          elsif @raw_list[i - 1].first == :del_elt
+            # previous block was del
+            deleted = @raw_list[i - 1][1]
+            added = @raw_list[i][2]
+            self << [:change_elt, deleted, added]
+          else
+            # previous block was common
+            self << block
           end
         else
           raise "the first element of the block #{i} is invalid: (#{block.first})\n"
